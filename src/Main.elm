489666ddff2f6351065659dc859c12a29a3bcaf9
Kim Tore Jensen <kimtjen@gmail.com>
2022-09-06 11:54:10 +0200

    request data for logged-in user on page load

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,43 @@
 module Main exposing (..)
 
+import Backend.Object
+import Backend.Object.User as User
+import Backend.Query as Query
+import Backend.Scalar exposing (Uuid)
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Browser.Navigation
+import Graphql.Http
+import Graphql.Operation exposing (RootQuery)
+import Graphql.SelectionSet exposing (SelectionSet, empty)
+import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+
 
 
 ---- MODEL ----
 
 
+type Actor
+    = LoggedIn UserData
+    | Unauthorized
+    | Unknown
+
+
 type alias Model =
-    {}
+    { user : Actor }
+
+
+type alias UserData =
+    { id : Uuid
+    , email : String
+    , name : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { user = Unknown }, sendGetMeRequest )
 
 
 
@@ -22,12 +45,21 @@ init =
 
 
 type Msg
-    = NoOp
+    = ClickedLogin
+    | GotMeResponse (Result (Graphql.Http.Error UserData) UserData)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        ClickedLogin ->
+            ( model, Browser.Navigation.load "http://localhost:3000/oauth2/login" )
+
+        GotMeResponse (Ok user) ->
+            ( { model | user = LoggedIn user }, Cmd.none )
+
+        GotMeResponse (Err _) ->
+            ( { model | user = Unauthorized }, Cmd.none )
 
 
 
@@ -37,13 +69,34 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "nais console" ]
+        , button [ onClick ClickedLogin ] [ text "Single sign-on" ]
         ]
 
 
 
 ---- PROGRAM ----
+
+
+sendGetMeRequest : Cmd Msg
+sendGetMeRequest =
+    getMeQuery
+        |> Graphql.Http.queryRequest "http://localhost:3000/query"
+        |> Graphql.Http.withCredentials
+        |> Graphql.Http.send GotMeResponse
+
+
+getMeQuery : SelectionSet UserData RootQuery
+getMeQuery =
+    Query.me meSelection
+
+
+meSelection : SelectionSet UserData Backend.Object.User
+meSelection =
+    Graphql.SelectionSet.map3 UserData
+        User.id
+        User.email
+        User.name
 
 
 main : Program () Model Msg
