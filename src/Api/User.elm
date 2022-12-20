da9499e66525102bbe39a760c221ed8fc9c651a4
Kim Tore Jensen <kimtjen@gmail.com>
2022-12-20 13:53:39 +0100

    Get personal team memberships from backend while querying current user

--- src/Api/User.elm
+++ src/Api/User.elm
@@ -1,7 +1,9 @@
 module Api.User exposing (..)
 
-import Backend.Object
+import Backend.Object exposing (TeamMembership(..))
 import Backend.Object.Role as Role
+import Backend.Object.Team
+import Backend.Object.TeamMembership as TeamMembership
 import Backend.Object.User as User
 import Backend.Query as Query
 import Backend.Scalar exposing (RoleName, Slug, Uuid)
@@ -22,10 +24,19 @@ type alias RoleData =
     }
 
 
+type alias TeamSlugData =
+    { slug : Slug }
+
+
+type alias TeamMembershipData =
+    { team : TeamSlugData }
+
+
 type alias UserData =
     { id : Uuid
     , email : String
     , name : String
+    , teamMemberships : List TeamMembershipData
     , roles : List RoleData
     }
 
@@ -42,11 +53,12 @@ getAllUsers =
 
 userDataWithRoleSelection : SelectionSet UserData Backend.Object.User
 userDataWithRoleSelection =
-    SelectionSet.map4 UserData
-        User.id
-        User.email
-        User.name
-        (User.roles roleDataSelection)
+    SelectionSet.succeed UserData
+        |> SelectionSet.with User.id
+        |> SelectionSet.with User.email
+        |> SelectionSet.with User.name
+        |> SelectionSet.with (User.teams teamMembershipSelection)
+        |> SelectionSet.with (User.roles roleDataSelection)
 
 
 userDataSelection : SelectionSet UserData Backend.Object.User
@@ -56,6 +68,7 @@ userDataSelection =
         |> SelectionSet.with User.email
         |> SelectionSet.with User.name
         |> SelectionSet.hardcoded []
+        |> SelectionSet.hardcoded []
 
 
 roleDataSelection : SelectionSet RoleData Backend.Object.Role
@@ -64,6 +77,18 @@ roleDataSelection =
         Role.name
         Role.isGlobal
         Role.targetTeamSlug
+
+
+teamMembershipSelection : SelectionSet TeamMembershipData Backend.Object.TeamMembership
+teamMembershipSelection =
+    SelectionSet.succeed TeamMembershipData
+        |> SelectionSet.with (TeamMembership.team teamDataSelection)
+
+
+teamDataSelection : SelectionSet TeamSlugData Backend.Object.Team
+teamDataSelection =
+    SelectionSet.succeed TeamSlugData
+        |> SelectionSet.with Backend.Object.Team.slug
 
 
 meSelection : SelectionSet (Maybe UserData) Backend.Union.AuthenticatedUser
