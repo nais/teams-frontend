4f89d655369e4ec986e8063e4260d07ab2270d30
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-03-27 12:01:46 +0200

    add navigation component

---
+++ src/Component/Navigation.elm
@@ -0,0 +1,61 @@
+module Component.Navigation exposing (view)
+
+import Api.Str exposing (slugStr)
+import Html exposing (Html, li, nav, text, ul)
+import Html.Attributes exposing (classList)
+import RemoteData exposing (RemoteData(..))
+import Route exposing (Route(..), link)
+import Session exposing (Session, Viewer(..))
+
+
+view : Route -> Session -> Html msg
+view route session =
+    case Session.viewer session of
+        LoggedIn _ ->
+            let
+                teamsButton : List (Html msg)
+                teamsButton =
+                    [ menuItem Route.MyTeams False "Teams" ]
+
+                adminButtons : List (Html msg)
+                adminButtons =
+                    if Session.isGlobalAdmin (Session.viewer session) then
+                        [ menuItem Route.ReconcilerAdmin False "Synchronizers"
+                        , menuItem Route.Users False "Users"
+                        ]
+
+                    else
+                        []
+
+                ephemeralButtons : List (Html msg)
+                ephemeralButtons =
+                    case route of
+                        Team slug ->
+                            [ menuItem route True (slugStr slug) ]
+
+                        DeleteTeam _ ->
+                            [ menuItem route True "Delete team" ]
+
+                        CreateTeam ->
+                            [ menuItem route True "Create team"
+                            ]
+
+                        _ ->
+                            []
+            in
+            nav [] [ ul [] (teamsButton ++ ephemeralButtons ++ adminButtons) ]
+
+        _ ->
+            nav [] []
+
+
+menuItem : Route -> Bool -> String -> Html.Html msg
+menuItem target indent title =
+    let
+        classes : List ( String, Bool )
+        classes =
+            [ ( "active", False )
+            , ( "indent", indent )
+            ]
+    in
+    li [ classList classes ] [ link target [ Html.Attributes.title title ] [ text title ] ]
