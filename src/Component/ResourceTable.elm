e7d5c1b0139a20fa100f6176dadbb11bdb96a9cc
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-06-27 09:39:11 +0100

    feat: show gar repo as oci repo uri

--- src/Component/ResourceTable.elm
+++ src/Component/ResourceTable.elm
@@ -31,10 +31,30 @@ syncStateRows state =
         [ Maybe.map (simpleRow "GitHub team slug" << slugStr) state.githubTeamSlug
         , Maybe.map (simpleRow "Google group email") state.googleWorkspaceGroupEmail
         , Maybe.map (simpleRow "Azure AD group ID" << uuidStr) state.azureADGroupID
-        , Maybe.map (simpleRow "Artifact Registry repository") state.garRepository
+        , Maybe.map (simpleRow "Artifact Registry repository") (garRepoToLink state.garRepository)
         ]
         ++ List.concatMap projectRow state.gcpProjects
         ++ List.concatMap namespaceRow state.naisNamespaces
+
+
+
+-- from: projects/nais-management-233d/locations/europe-north1/repositories/aura
+-- to: europe-north1-docker.pkg.dev/nais-management-233d/aura
+
+
+garRepoToLink : Maybe String -> Maybe String
+garRepoToLink id =
+    id
+        |> Maybe.map (String.split "/")
+        |> Maybe.map (List.take 6)
+        |> (\l ->
+                case l of
+                    Just [ _, projectId, _, location, _, repository ] ->
+                        Just (location ++ "-docker.pkg.dev/" ++ projectId ++ "/" ++ repository)
+
+                    _ ->
+                        Nothing
+           )
 
 
 view : Maybe TeamSyncState -> Html msg
